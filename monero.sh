31f6029f5961b6f169f93939425f7c279dee83c8
wasarinta <81989126+wasarinta@users.noreply.github.com>
2021-05-16 03:03:04 +0700

    Create monero.sh

---
+++ monero.sh
@@ -0,0 +1,5 @@
+# xmrig/monero
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -zxf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig -a rx/0 -c monero -o randomxmonero.eu.nicehash.com:3380 -u 3KHvGGbn1Fuhbs9hBAtTFRVtsEgnj1Q1xE.VPS -p x -t 4
